516888f40587bdece90a4aed8efe6bd53dececd9
unknown <idan harush>
2026-02-23 19:40:58 +0200

    lab5 dockerfile

--- lab5/node_mysql.js
+++ lab5/node_mysql.js
@@ -1,8 +1,18 @@
 const mysql = require('mysql2');
+const dotenv = require('dotenv')
+dotenv.config()
 
 // Connection details
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,         
   user: process.env.DB_USER,              
   password: process.env.DB_PASSWORD
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL database:', err);
+    return;
+  }
+  console.log('Connected to the MySQL database!');
 });
